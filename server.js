6ea80fae199d0d789d29a077695320a80ce6f90c
shrikantdottechie <106555771+shrikantdottechie@users.noreply.github.com>
2022-11-30 23:12:35 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
+const magics = require('./models/magics.js');
 // Create our express app
 const app = express();
-
+const port = process.env.PORT || 3000;
 // Define a "root" route directly on app
 // Tomorrow, we'll use best practice routing
 app.get('/greeting', function (req, res) {
@@ -23,9 +23,19 @@ app.get('/greeting', function (req, res) {
             console.log(req.params);
             res.send(`Tip amount on ${req.params.total} in percentage is: ${req.params.tipPercentage}`);
           });
+//magics[Math.floor(Math.random() * magics.length)]
+
+    /*app.get('/magics, function (req, res) {
+     res.send('<h1>Will I be a Millionaire </h1>'+ `${magics}`);
+    });*/
+
+
+    app.get('/magic/Will%20I%20Be%20A%20Millionaire', function (req, res) {
+    res.send('<h1>Will I be a Millionaire </h1>'+ `${magics[Math.floor(Math.random() * magics.length)]}`);
+    });
 
 // Tell the app to listen on port 3000
 // for HTTP requests from clients
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Listening on port 3000');
 });
